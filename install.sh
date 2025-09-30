a56408fe9b8590adc543d6c1dbda3f1dd74d392e
DIDA <adidapermana7@gmail.com>
2025-09-30 07:08:15 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -114,7 +114,7 @@ function pointing() (
 
     apt update
     apt install jq curl -y
-    DOMAIN=dida.it.com
+    DOMAIN=gachorr.web.id
     generate_random_subdomain
     dns=${sub}.${DOMAIN}
     CF_KEY=5c03942dfbe00eb8dc2f2ac83fde32ece8aed
