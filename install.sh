63caac93bafed0c1a988020f15f7265557f622a0
DIDA <adidapermana7@gmail.com>
2025-10-20 10:27:51 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -999,7 +999,7 @@ cat > /etc/xray/config.json << END
            "clients": [
               {
                  "password": "${uuid}"
-#trojan-ws
+#trojanws
               }
           ],
          "udp": true
@@ -1007,7 +1007,7 @@ cat > /etc/xray/config.json << END
        "streamSettings":{
            "network": "ws",
            "wsSettings": {
-               "path": "/trojan"
+               "path": "/trojan-ws"
             }
          }
      },
