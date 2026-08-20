4cfc85cd00ae6f05c75d055fdc783d0ec7cd5e3a
ASEP DIDA PERMANA <adidapermana7@gmail.com>
2026-08-20 20:50:01 +0700

    Download and set up addhost and fixcert scripts
    Added scripts to update hostname and fix certificates from the latest source.

--- menu/update.sh
+++ menu/update.sh
@@ -11,6 +11,12 @@ chmod +x menu/*
 mv menu/* /usr/local/sbin
 rm -rf menu menu.zip
 
+# Menu change-domain selalu diambil dari source terbaru agar hostname API ikut tersinkron.
+wget -q -O /usr/local/sbin/addhost https://raw.githubusercontent.com/Adidastore11/P/main/menu/menu/addhost
+chmod +x /usr/local/sbin/addhost
+wget -q -O /usr/local/sbin/fixcert https://raw.githubusercontent.com/Adidastore11/P/main/menu/menu/fixcert
+chmod +x /usr/local/sbin/fixcert
+
 # Mengunduh versionbaru dan menggantinya menjadi "version"
 wget -q https://raw.githubusercontent.com/Adidastore11/P/main/versionbaru -O /usr/local/sbin/version
 chmod +x /usr/local/sbin/version
