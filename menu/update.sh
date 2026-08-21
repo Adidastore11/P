85ca2699bf1aea33744fab5791544dc7ed1aed57
ASEP DIDA PERMANA <adidapermana7@gmail.com>
2026-08-21 16:25:28 +0700

    Update update.sh

--- menu/update.sh
+++ menu/update.sh
@@ -7,15 +7,13 @@ apt install -y unzip wget
 # Mengunduh dan mengekstrak menu.zip
 wget -q https://raw.githubusercontent.com/Adidastore11/P/main/menu/menu.zip -O menu.zip
 unzip menu.zip
+if [[ ! -f menu/addhost || ! -f menu/fixcert ]]; then
+  echo "menu.zip tidak lengkap: addhost atau fixcert tidak ditemukan."
+  exit 1
+fi
 chmod +x menu/*
 mv menu/* /usr/local/sbin
 rm -rf menu menu.zip
-
-# Menu change-domain selalu diambil dari source terbaru agar hostname API ikut tersinkron.
-wget -q -O /usr/local/sbin/addhost https://raw.githubusercontent.com/Adidastore11/P/main/menu/menu/addhost
-chmod +x /usr/local/sbin/addhost
-wget -q -O /usr/local/sbin/fixcert https://raw.githubusercontent.com/Adidastore11/P/main/menu/menu/fixcert
-chmod +x /usr/local/sbin/fixcert
 
 # Mengunduh versionbaru dan menggantinya menjadi "version"
 wget -q https://raw.githubusercontent.com/Adidastore11/P/main/versionbaru -O /usr/local/sbin/version
